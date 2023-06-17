de67c5a6152602f6e551eb9f93693f375c2fcfc7
Juan_Victor <estudantejuan0@gmail.com>
2023-06-17 18:22:30 -0300

    aula 08

--- src/aula-07.ts
+++ src/aula-07.ts
@@ -3,12 +3,11 @@ const funcaoCallback = (): void => {
   console.log("Deu bom");
 };
 
-const funcaoTeste2 = (valor1: number, valor2: number, callback: () => void ): string => {
-    if (valor1 > valor2) {
-        callback();
-    }
-    return "";
+const funcaoTeste2 = (valor1: number, valor2: number, callback: () => void): string => {
+  if (valor1 > valor2) {
+    callback();
+  }
+  return "";
 };
 
-
-funcaoTeste2(54, 32, funcaoCallback)
+funcaoTeste2(54, 32, funcaoCallback);
---
+++ src/aula-08.ts
@@ -0,0 +1,40 @@
+/* eslint-disable prettier/prettier */
+class Outro {
+    endereco: string;
+
+    constructor(){
+        this.endereco = ''
+    }
+}
+
+interface Carro {
+  idade: number;
+  nome: string;
+}
+
+interface Bicicleta {
+    rodas: number;
+}
+
+const funcao = (validacao: boolean): Carro | Bicicleta=> {
+    if(validacao) {
+        return {
+            idade: 32,
+            nome: "Gol",
+        };
+    }
+    return {
+        rodas: 4,
+    };
+};
+
+
+const resultado = funcao(false);
+
+if ("idade" in resultado) {
+    console.log(resultado.nome);
+}
+
+if ("rodas" in resultado) {
+    console.log(resultado.rodas)
+}
