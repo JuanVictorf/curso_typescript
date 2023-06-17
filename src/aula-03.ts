8243a941644ff16c54923cecab374cea0a402057
Juan_Victor <estudantejuan0@gmail.com>
2023-06-17 17:04:44 -0300

    aula 03

---
+++ src/aula-03.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+interface Pessoa {
+  idade: number;
+  nome: string;
+  isMaiorIdade: boolean;
+
+}
+
+const pessoa: Pessoa = {
+  idade: 543,
+  nome: "João",
+  isMaiorIdade: true,
+};
+
+console.log(pessoa);
+console.log("Idade da pessoa: ",  pessoa.idade)
